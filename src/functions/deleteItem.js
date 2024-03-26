f5ef7063c4a6ee693fe34bb4e745d2450f9d6894
Raphael Beer <raphael.beer@ict-campus.net>
2024-03-26 16:21:55 +0100

    Publish unfinished DeleteItem/PutItem

--- src/functions/deleteItem.js
+++ src/functions/deleteItem.js
@@ -1,23 +1,30 @@
-const { app,output } = require('@azure/functions');
 
-const cosmosOutput = output.cosmosDB({
-    databaseName: 'DemoDatabase',
-    containerName: 'DemoContainer1',
-    connection: 'CosmosDB',
-    createIfNotExists: true
-});
+const { app } = require('@azure/functions');
+const { CosmosClient } = require("@azure/cosmos");
+
+const endpoint = "https://dbcosmoscampus.documents.azure.com:443";
+const key = "blabla";
+const client = new CosmosClient({ endpoint, key });
+
+const databaseId = "DemoDatabase";
+const containerId = "DemoContainer1";
 
 app.http('deleteItem', {
     methods: ['DELETE'],
     authLevel: 'anonymous',
-    extraOutputs: [cosmosOutput],
-    route: 'items',
-    handler: async (request, context) => {
-        const data = await request.json();
-        context.extraOutputs.set(cosmosOutput, data);
-        return {
-            body: JSON.stringify(data),
-            status: 201
+    route: 'items/{id}',
+    handler: async (myTimer, context) => {
+        const timeStamp = new Date().toISOString();
+
+        if (myTimer.isPastDue) {
+            context.log('JavaScript is running late!');
         }
+        context.log('JavaScript timer trigger function ran!', timeStamp);   
+
+        const database = client.database(databaseId);
+        const container = database.container(containerId);
+
+        const itemId = context.options.trigger.route; // The id of the item you want to delete
+        const { resource: deletedItem } = await container.item(itemId, itemId).delete();
     }
 });
