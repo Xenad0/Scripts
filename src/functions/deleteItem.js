8991bea046e741ffdfe7702de28e60273389a08b
Raphael Beer <raphael.beer@ict-campus.net>
2024-04-03 11:26:40 +0200

    Deletefunction is Working

--- src/functions/deleteItem.js
+++ src/functions/deleteItem.js
@@ -3,7 +3,7 @@ const { app } = require('@azure/functions');
 const { CosmosClient } = require("@azure/cosmos");
 
 const endpoint = "https://dbcosmoscampus.documents.azure.com:443";
-const key = "blabla";
+const key = "KEY";
 const client = new CosmosClient({ endpoint, key });
 
 const databaseId = "DemoDatabase";
@@ -13,18 +13,17 @@ app.http('deleteItem', {
     methods: ['DELETE'],
     authLevel: 'anonymous',
     route: 'items/{id}',
-    handler: async (myTimer, context) => {
+    handler: async (request) => {
         const timeStamp = new Date().toISOString();
 
-        if (myTimer.isPastDue) {
-            context.log('JavaScript is running late!');
-        }
-        context.log('JavaScript timer trigger function ran!', timeStamp);   
+        console.log(request);
+        console.log("-------");
+        console.log(request.params.id);
 
         const database = client.database(databaseId);
         const container = database.container(containerId);
 
-        const itemId = context.options.trigger.route; // The id of the item you want to delete
+        const itemId = request.params.id; // The id of the item you want to delete
         const { resource: deletedItem } = await container.item(itemId, itemId).delete();
     }
 });
